c19a20c54745e7b1f2dfdc16d84d9a10cd8a8911
geektechniquestudios <geektechniquestudios@gmail.com>
2020-10-06 17:01:20 -0400

    added beginnings of react native client, very early

---
+++ ThermostatReactNative/App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect, useState} from 'react';
+import {RNFluidicSlider} from 'react-native-fluidic-slider';
+import LinearGradient from 'react-native-linear-gradient';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  const [temperature, setTemperature] = useState(70); //
+  // useEffect(() => {
+  //   fetch("10.0.0.20") //get temp from spring boot server
+  // })
+
+  function handleNewTemp(position) {
+    setTemperature(Math.trunc(position * 20 + 60));
+    //send request to spring boot server
+  }
+
+  return (
+    <>
+      <View style={styles.slider}>
+        <RNFluidicSlider
+          min={60}
+          max={80}
+          initialPosition={(temperature - 60) / 20}
+          bubbleColor="blue" //make same as bg
+          barColor="black" //add temp color func
+          bubbleTextColor="white"
+          endTracking={(position) => handleNewTemp(position)}
+        />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: Colors.gray,
+  },
+  slider: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+});
+
+export default App;
